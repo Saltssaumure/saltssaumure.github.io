b53c6c66e6c4bc00fa03e1b187c2f2c74ffd1f88
Saltssaumure <randomized.goldfish@gmail.com>
2020-09-01 18:06:12 +0100

    piOStextbox add

---
+++ texteditor/piOStextjs.js
@@ -0,0 +1,9 @@
+function help() {
+    var ok = document.getElementById("helpful");
+    if (ok.style.display === "block") {
+        ok.style.display = "none";
+    }
+    else {
+        ok.style.display = "block";
+    }
+}
