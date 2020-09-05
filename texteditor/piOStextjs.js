9c73df9c045ac43ba662d223b3378f01cfdb17e3
Saltssaumure <randomized.goldfish@gmail.com>
2020-09-05 19:47:48 +0100

    deleted duplicate of pios-text-editor

--- texteditor/piOStextjs.js
+++
@@ -1,9 +0,0 @@
-function help() {
-    var ok = document.getElementById("helpful");
-    if (ok.style.display === "block") {
-        ok.style.display = "none";
-    }
-    else {
-        ok.style.display = "block";
-    }
-}
